ee0063301fbb831206d2be3a1adaeec163702bdc
Rorisang Putu <rorisangputu@gmail.com>
2024-09-27 01:38:34 +0200

    adding portfolio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,17 +1,56 @@
 import React from 'react'
+import Button from './Button'
+import law from '../assets/LawCorp.jpeg';
+import handle from '../assets/WeHandle.jpeg';
 
 const Portfolio = () => {
     return (
         <div className='py-10'>
-            <div className='w-[90%] mx-auto'>
+            <div className='w-[90%] mx-auto flex flex-col gap-10'>
                 <div className='flex flex-col items-center text-center gap-4'>
                     <p className='font-light text-xl'>Our portfolio</p>
                     <h1 className='text-[40px] font-medium'>Our work from the past year</h1>
-                    <p className='w-[700px]'>
+                    <p className='max-w-3xl text-gray-600 text-lg'>
                         Our portfolio demonstrates our expertise in solving complex design
                         challenges, creating seamless user flows, and delivering visually stunning
                         interfaces. Let us bring our design prowess to your project.
                     </p>
+                </div>
+                <div className='grid grid-cols-2 gap-10'>
+                    <div className='flex flex-col justify-around items-center shadow-md rounded-lg py-10 p gap-4'>
+                        <div className='py-5 flex flex-col gap-4'>
+                            <div className='flex flex-col gap-2 '>
+                                <h1 className='text-4xl'>Law Corp Inc</h1>
+                                <p className='text-md max-w-xl'>Law Corp is a mock law firm website that we created to showcase
+                                    our ability to create fully functional and engaging corprate websites
+                                </p>
+                            </div>
+                            <a href="#" className='text-left'>
+                                <Button title="View" styles="text-blue bg-transparent border border-blue
+                            rounded-lg py-2 px-7 hover:bg-blue hover:text-white " />
+                            </a>
+                            <div className='h-[300px] w-[600px]'>
+                                <img src={law} alt="" className='' />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col justify-around items-center shadow-md rounded-lg py-10 p gap-4'>
+                        <div className='py-5 flex flex-col gap-4'>
+                            <div className='flex flex-col gap-2 '>
+                                <h1 className='text-4xl'>Law Corp Inc</h1>
+                                <p className='text-md max-w-xl'>Law Corp is a mock law firm website that we created to showcase
+                                    our ability to create fully functional and engaging corprate websites
+                                </p>
+                            </div>
+                            <a href="#" className='text-left'>
+                                <Button title="Live" styles="text-blue bg-transparent border border-blue
+                            rounded-lg py-2 px-7 hover:bg-blue hover:text-white " />
+                            </a>
+                            <div className='h-[300px] w-[600px]'>
+                                <img src={law} alt="" className='' />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
