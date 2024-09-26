bc95397ea97cbfa8857c3279964490f799b3fbe8
Rorisang Putu <rorisangputu@gmail.com>
2024-09-26 23:55:45 +0200

    styling nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import Logo from '../assets/LogoLerumo.png'
+import Button from './Button'
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className="bg-bgsecondary">
+            <div className="w-[90%] mx-auto py-3 flex justify-between items-center">
+                <div className='flex items-center'>
+                    <img src={Logo} alt="" className='w-24' />
+                </div>
+                {/* Nav Links */}
+                <div className='hidden lg:block'>
+                    <ul className='flex items-center gap-3'>
+                        {navlinks.map((links) => (
+                            <li key={links.id}>
+                                <a href={links.link} className='font-semibold text-lg inline-block py-2 px-3 
+                                hover:text-[#414141] relative group hover:scale-105 duration-200 '>
+                                    {links.title}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                {/* Get In Touch Button */}
+                <Button title="Get In Touch"
+                    styles="hidden lg:block bg-blue 
+                    text-white rounded-lg p-3 hover:bg-[#3880c9] hover:scale-105 duration-200"
+                />
+                <HiOutlineMenuAlt3 className='w-8 h-8 lg:hidden text-black' />
+
+            </div>
+
+        </nav>
     )
 }
+
+const navlinks = [
+    {
+        id: 1,
+        title: "Home",
+        link: "#"
+    },
+    {
+        id: 2,
+        title: "About",
+        link: "#"
+    },
+    {
+        id: 3,
+        title: "Services",
+        link: "#"
+    },
+]
 
 export default Navbar
