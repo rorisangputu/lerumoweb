93042d268953dff2528e152e9b3863f33bcd525a
Rorisang Putu <rorisangputu@gmail.com>
2024-09-27 01:19:50 +0200

    changing text size

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -4,14 +4,13 @@ const Portfolio = () => {
     return (
         <div className='py-10'>
             <div className='w-[90%] mx-auto'>
-                <div className='flex flex-col items-center text-center'>
-                    <p className='font-light'>Our portfolio</p>
+                <div className='flex flex-col items-center text-center gap-4'>
+                    <p className='font-light text-xl'>Our portfolio</p>
                     <h1 className='text-[40px] font-medium'>Our work from the past year</h1>
-                    <p className='w-[600px]'>
+                    <p className='w-[700px]'>
                         Our portfolio demonstrates our expertise in solving complex design
                         challenges, creating seamless user flows, and delivering visually stunning
                         interfaces. Let us bring our design prowess to your project.
-
                     </p>
                 </div>
             </div>
