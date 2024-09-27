53e851be5ae7c0785938a97ee70147ebd15be6f4
Rorisang Putu <rorisangputu@gmail.com>
2024-09-28 00:48:16 +0200

    changing shadow

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -17,35 +17,35 @@ const Portfolio = () => {
                     </p>
                 </div>
                 <div className='grid grid-cols-2 gap-10'>
-                    <div className='flex flex-col justify-around items-center shadow-md bg-[#ececec] rounded-lg py-10 p gap-4'>
+                    <div className='flex flex-col justify-around items-center drop-shadow-lg bg-[#ececec] rounded-lg py-10 p gap-4'>
                         <div className='py-5 flex flex-col gap-4'>
                             <div className='flex flex-col gap-2 '>
                                 <h1 className='text-4xl font-semibold'>Law Corp Inc</h1>
-                                <p className='text-md max-w-xl'>Law Corp is a mock law firm website that we created to showcase
+                                <p className='text-md max-w-xl text-gray-600'>Law Corp is a mock law firm website that we created to showcase
                                     our ability to create fully functional and engaging corprate websites
                                 </p>
                             </div>
                             <a href="https://law-corp-09.vercel.app/" className='text-left'>
                                 <Button title="View" styles="text-blue bg-transparent border border-blue
-                            rounded-lg py-2 px-7 hover:bg-blue hover:text-white " />
+                            rounded-lg py-3 px-10 hover:bg-blue hover:text-white " />
                             </a>
                             <div className='h-[335px]] w-[600px]'>
                                 <img src={law} alt="" className='h-full w-full object-cover' />
                             </div>
                         </div>
                     </div>
-                    <div className='flex flex-col justify-around items-center shadow-md bg-[#ececec] rounded-lg py-10 p gap-4'>
+                    <div className='flex flex-col justify-around items-center drop-shadow-lg bg-[#ececec] rounded-lg py-10 p gap-4'>
                         <div className='py-5 flex flex-col gap-4'>
                             <div className='flex flex-col gap-2 '>
                                 <h1 className='text-4xl font-semibold'>Sound Tour</h1>
-                                <p className='text-md max-w-xl'>
+                                <p className='text-md max-w-xl text-gray-600'>
                                     We developed a streaming link directory website for a media agency who create
                                     digital marketing solutions for brands in various industries.
                                 </p>
                             </div>
                             <a href="https://www.soundtour.co.za" className='text-left'>
                                 <Button title="View" styles="text-blue bg-transparent border border-blue
-                            rounded-lg py-2 px-7 hover:bg-blue hover:text-white " />
+                            rounded-lg py-3 px-10 hover:bg-blue hover:text-white " />
                             </a>
                             <div className='h-[335px] w-[600px]'>
                                 <img src={soundtour} alt="" className='h-full w-full object-cover' />
