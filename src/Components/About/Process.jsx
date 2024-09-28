081cc0e7d330d2ccb8bad5de92803df658776e29
Rorisang Putu <rorisangputu@gmail.com>
2024-09-28 04:28:29 +0200

    creating process page

---
+++ src/Components/About/Process.jsx
@@ -0,0 +1,19 @@
+
+
+const Process = () => {
+    return (
+        <div className="bg-slate-50 py-10">
+            <div className="w-[90%] mx-auto">
+                <div className="flex flex-col lg:flex-row">
+                    <div className="w-1/2">
+                        <h1>Process</h1>
+                        <p></p>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Process
