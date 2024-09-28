515e496550846e42c2a0c8353ebbdd66f6395ae9
Rorisang Putu <rorisangputu@gmail.com>
2024-09-28 02:44:40 +0200

    configuring routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom';
 import Navbar from "./Components/Navbar"
 import Home from "./Pages/Home"
 import Footer from './Components/Footer'
@@ -9,7 +10,19 @@ function App() {
   return (
     <div className="overflow-x-hidden bg-bg">
       <Navbar />
-      <Home />
+      <div className="flex-grow">
+        <Routes>
+          <Route path={'/'} element={
+            <Home />
+          } />
+          <Route path={'/'} element={
+            <Home />
+          } />
+          <Route path={'/'} element={
+            <Home />
+          } />
+        </Routes>
+      </div>
       <Footer />
     </div>
   )
