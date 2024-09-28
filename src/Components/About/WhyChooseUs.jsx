3013087b2ba4ee426e1f39806967480c28405191
Rorisang Putu <rorisangputu@gmail.com>
2024-09-28 04:22:58 +0200

    changing text size

--- src/Components/About/WhyChooseUs.jsx
+++ src/Components/About/WhyChooseUs.jsx
@@ -10,7 +10,7 @@ const WhyChooseUs = () => {
                 <div className="flex flex-col gap-7">
                     <div className="flex flex-col justify-center items-center gap-8">
                         <h1 className="text-[65px] font-semibold">Why Choose Us</h1>
-                        <p className="text-xl text-gray-600">
+                        <p className="text-2xl text-gray-600">
                             Our experience, skills, and dedication set us apart and make us the
                             ideal choice for your project
                         </p>
@@ -21,8 +21,8 @@ const WhyChooseUs = () => {
                             rounded-full'>
                                 <img src={expert} alt="" />
                             </div>
-                            <h1 className="font-light text-2xl">Expertise</h1>
-                            <p className="text-gray-600 text-lg max-w-2xl">
+                            <h1 className="font-light text-3xl">Expertise</h1>
+                            <p className="text-gray-600 text-2xl max-w-3xl">
                                 We have extensive experience in designing and developing website
                                 clients across various industries
                             </p>
@@ -32,7 +32,7 @@ const WhyChooseUs = () => {
                             rounded-full'>
                                 <img src={veri} alt="" />
                             </div>
-                            <h1 className="font-light text-2xl">Expertise</h1>
+                            <h1 className="font-light text-3xl">Expertise</h1>
                             <p className="text-gray-600 text-lg max-w-2xl">
                                 We have extensive experience in designing and developing website
                                 clients across various industries
@@ -43,7 +43,7 @@ const WhyChooseUs = () => {
                             rounded-full'>
                                 <img src={comm} alt="" />
                             </div>
-                            <h1 className="font-light text-2xl">Expertise</h1>
+                            <h1 className="font-light text-3xl">Expertise</h1>
                             <p className="text-gray-600 text-lg max-w-2xl">
                                 We have extensive experience in designing and developing website
                                 clients across various industries
@@ -54,7 +54,7 @@ const WhyChooseUs = () => {
                             rounded-full'>
                                 <img src={cust} alt="" />
                             </div>
-                            <h1 className="font-light text-2xl">Expertise</h1>
+                            <h1 className="font-light text-3xl">Expertise</h1>
                             <p className="text-gray-600 text-lg max-w-2xl">
                                 We have extensive experience in designing and developing website
                                 clients across various industries
