2bb16c2c66f96775c4a1c58f8eed28dc9508f2fa
Rorisang Putu <rorisangputu@gmail.com>
2024-09-28 03:34:12 +0200

    adding about us hero styling and content

---
+++ src/Components/About/Hero.jsx
@@ -0,0 +1,16 @@
+
+
+const Hero = () => {
+    return (
+        <div className="hero-bg py-20">
+            <div className="w-[90%] mx-auto py-24">
+                <div className="flex flex-col justify-center items-center gap-7">
+                    <h1 className="text-[65px] font-semibold">About Us</h1>
+                    <p className="font-light text-lg text-gray-500">Home/About Us</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
