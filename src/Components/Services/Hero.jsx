e48e6810a79767544da52b58613de59dd0a82da4
Rorisang Putu <rorisangputu@gmail.com>
2024-10-01 01:53:37 +0200

    changing files

---
+++ src/Components/Services/Hero.jsx
@@ -0,0 +1,18 @@
+
+
+const Hero = () => {
+    return (
+        <div className="hero-bg py-20">
+            <div className="w-[90%] mx-auto py-24">
+                <div className="flex flex-col justify-center items-center gap-7">
+                    <h1 className="text-[65px] font-semibold">Our Services</h1>
+                    <p className="font-light text-lg text-gray-500">
+                        <span className="text-black font-semibold">Home / </span>Services
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
