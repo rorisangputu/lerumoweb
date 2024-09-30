e639128f58bc7e5baf17b2fdd29923d3d2b71d35
Rorisang Putu <rorisangputu@gmail.com>
2024-10-01 01:53:19 +0200

    making changes to button

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import Button from "../Button"
+
+
+const Hero = () => {
+    return (
+        <section className="hero-bg">
+            <div className="w-[90%] mx-auto py-36 flex flex-col 
+                    justify-center items-center gap-5 text-center">
+                <h1 className="text-[65px] !leading-snug font-bold 
+                    w-[700px]">
+                    Expert Web Development Services for Your Business
+                </h1>
+                <p className="w-[55%] font-light">
+                    Partner with our Experienced Team of Web Developers
+                    to Build Your Brand&apos;s Unique Online Identity with Custom
+                    Web Design, and Web Applications that Deliver Results
+                </p>
+                <Button
+                    title="Get in touch"
+                    styles="bg-blue 
+                    text-white rounded-lg p-3 hover:bg-[#3880c9]
+                    hover:scale-105 duration-200"
+                />
+            </div>
+        </section>
+    )
+}
+
+export default Hero
