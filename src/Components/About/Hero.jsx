45e4dcad80d0731cc28f115d2f19f8233532de4a
Rorisang Putu <rorisangputu@gmail.com>
2024-10-01 01:53:00 +0200

    changing files

--- src/Components/About/Hero.jsx
+++ src/Components/About/Hero.jsx
@@ -6,7 +6,9 @@ const Hero = () => {
             <div className="w-[90%] mx-auto py-24">
                 <div className="flex flex-col justify-center items-center gap-7">
                     <h1 className="text-[65px] font-semibold">About Us</h1>
-                    <p className="font-light text-lg text-gray-500">Home/About Us</p>
+                    <p className="font-light text-lg text-gray-500">
+                        <span className="text-black font-semibold">Home / </span>About Us
+                    </p>
                 </div>
             </div>
         </div>
