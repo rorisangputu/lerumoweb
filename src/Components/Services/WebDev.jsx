4e19eb4d6b09271da7f273913c4846583a72994e
Rorisang Putu <rorisangputu@gmail.com>
2024-10-02 17:29:31 +0200

    changing text sizes

--- src/Components/Services/WebDev.jsx
+++ src/Components/Services/WebDev.jsx
@@ -8,10 +8,10 @@ const WebDev = () => {
                 <div className='flex flex-col lg:flex-row items-center
                 justify-center gap-8'>
                     <div className='lg:w-1/2 flex flex-col items-center text-center lg:text-start lg:items-start gap-5'>
-                        <h1 className='text-4xl px-10 lg:px-0 leading-none 
-                            lg:text-[65px] font-semibold lg:leading-[5rem]'
+                        <h1 className='text-3xl px-10 lg:px-0 leading-none 
+                            lg:text-6 font-semibold lg:leading-[5rem]'
                         >Web & App Development Done Differently</h1>
-                        <p className='max-w-2xl text-xl text-gray-600'>
+                        <p className='max-w-2xl text-lg md:text-xl text-gray-600'>
                             We meticulously write custom code line by line to our customers
                             specifications, giving us total control over the design, and
                             performance of the website.
@@ -19,7 +19,7 @@ const WebDev = () => {
                         <Button
                             title={"Get in touch"}
                             styles="text-blue text-lg hover:text-white border
-                            border-blue rounded-xl py-4 px-3 hover:bg-[#3880c9]
+                            border-blue rounded-xl py-3 md:py-4 px-3 hover:bg-[#3880c9]
                             hover:scale-105 duration-200"
                         />
                     </div>
