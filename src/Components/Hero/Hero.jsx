284b975b583c0121de8f99aafdd0375af212793e
Rorisang Putu <rorisangputu@gmail.com>
2024-10-02 15:37:50 +0200

    making hero mobile responsive

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,13 +4,13 @@ import Button from "../Button"
 const Hero = () => {
     return (
         <section className="hero-bg">
-            <div className="w-[90%] mx-auto py-36 flex flex-col 
+            <div className="w-[90%] mx-auto py-20 lg:py-36 flex flex-col 
                     justify-center items-center gap-5 text-center">
-                <h1 className="text-[65px] !leading-snug font-bold 
-                    w-[700px]">
+                <h1 className="text-4xl md:text-5xl lg:text-[65px] !leading-snug font-bold 
+                    lg:w-[700px]">
                     Expert Web Development Services for Your Business
                 </h1>
-                <p className="w-[55%] font-light">
+                <p className="text-sm md:text-md lg:w-[55%] lg:text-xl font-light">
                     Partner with our Experienced Team of Web Developers
                     to Build Your Brand&apos;s Unique Online Identity with Custom
                     Web Design, and Web Applications that Deliver Results
@@ -18,7 +18,7 @@ const Hero = () => {
                 <Button
                     title="Get in touch"
                     styles="bg-blue 
-                    text-white rounded-lg p-3 hover:bg-[#3880c9]
+                    text-white rounded-lg text-md md:text-lg px-6 py-3 md:py-4 hover:bg-[#3880c9]
                     hover:scale-105 duration-200"
                 />
             </div>
