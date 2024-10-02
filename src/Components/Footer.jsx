9c0968104925d81d44cc143567bc57ee59e892dc
Rorisang Putu <rorisangputu@gmail.com>
2024-10-02 16:38:05 +0200

    styling for mobile

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className='bg-gray-100 py-7'>
             <div className='w-[90%] mx-auto'>
                 <div className='flex flex-col gap-3'>
-                    <div className='flex justify-between items-center'>
+                    <div className='flex flex-col lg:flex-row justify-between items-center'>
                         <div className='h-32 w-32'>
                             <img src={Logo} alt="" className='w-full h-full' />
                         </div>
@@ -16,7 +16,7 @@ const Footer = () => {
                             <a href="/services" className='text-lg hover:text-gray-400'>Services</a>
                         </div>
                     </div>
-                    <div className='flex justify-between'>
+                    <div className='flex flex-col lg:flex-row items-center justify-between'>
                         <p className='text-lg font-medium text-gray-600'>&copy; 2024 Lerumo Tech. All rights reserved.</p>
                         <div className='flex gap-7'>
                             <a href=""><FaFacebook className='h-7 w-7' /></a>
