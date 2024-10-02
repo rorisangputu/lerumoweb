ba8a8b336b5d6d97c58ed9395e6e38ad7b6db166
Rorisang Putu <rorisangputu@gmail.com>
2024-10-02 23:24:55 +0200

    creating contact page

---
+++ src/Pages/Contacts.jsx
@@ -0,0 +1,12 @@
+
+import ContactsForm from '../Components/Contacts/ContactsForm'
+
+const Contacts = () => {
+    return (
+        <div>
+            <ContactsForm />
+        </div>
+    )
+}
+
+export default Contacts
