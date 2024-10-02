7e9f13c0270e2c297992ebb3e4a108240bd24a3a
Rorisang Putu <rorisangputu@gmail.com>
2024-10-03 01:20:31 +0200

    changing layout

--- src/Components/Hero/Services.jsx
+++ src/Components/Hero/Services.jsx
@@ -1,60 +1,19 @@
-import code from '../../assets/code.png';
-import SocialMedia from '../../assets/SocialMedia.png';
-import WebAppImage from '../../assets/WebAppImage.png';
+// import code from '../../assets/code.png';
+// import SocialMedia from '../../assets/SocialMedia.png';
+// import WebAppImage from '../../assets/WebAppImage.png';
+
+import Accordian from "../Accordian";
+
 
 const Services = () => {
     return (
         <div className='py-20 bg-gray-100'>
             <div className='w-[90%] mx-auto'>
                 <div className='flex flex-col-reverse items-center lg:flex-row gap-6'>
-                    {/* Services */}
-                    <div className="lg:w-1/2 flex flex-col gap-5">
-                        <div className='bg-white flex items-center p-6 gap-5 rounded-xl drop-shadow-md
-                            hover:drop-shadow-xl hover:scale-105 duration-200'>
-                            <div className='w-1/3 h-10 lg:h-20 lg:w-20'>
-                                <img src={code} alt="" className='object-cover w-full h-full' />
-                            </div>
-                            <div className='flex flex-col gap-3'>
-                                <h1 className='text-2xl lg:text-3xl font-semibold'>Custom Website Development</h1>
-                                <p className='text-lg lg:text-xl max-w-2xl font-light'>
-                                    We create fully custom, responsive websites that deliver exceptional
-                                    user experiences. Our goal is to build solutions that meet the unique
-                                    needs of your business while ensuring high performance and scalability.
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className='bg-blue text-white flex items-center p-6 gap-5 
-                                rounded-xl drop-shadow-md hover:drop-shadow-xl hover:scale-105 duration-200'>
-                            <div className='w-1/3 h-10 lg:h-20 lg:w-20'>
-                                <img src={WebAppImage} alt="" className='object-cover w-full h-full' />
-                            </div>
-                            <div className='flex flex-col gap-3'>
-                                <h1 className='text-2xl lg:text-3xl font-semibold'>Web Application Development</h1>
-                                <p className='text-lg lg:text-xl max-w-2xl font-light'>
-                                    We develop powerful, scalable web applications that help businesses streamline
-                                    operations and improve efficiency. From user-friendly interfaces to robust
-                                    backend solutions, we tailor every aspect to your business needs.
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className='bg-white flex items-center p-6 gap-5 rounded-xl drop-shadow-md
-                                hover:drop-shadow-xl hover:scale-105 duration-200'>
-                            <div className='w-1/3 h-10 lg:h-20 lg:w-20'>
-                                <img src={SocialMedia} alt="" className='object-cover w-full h-[80%]' />
-                            </div>
-                            <div className='flex flex-col gap-3'>
-                                <h1 className='text-2xl lg:text-3xl font-semibold'>Social Media Strategy & Setup</h1>
-                                <p className='text-lg lg:text-xl max-w-2xl font-light'>
-                                    We provide expert social media setup and strategy services, helping you
-                                    establish a strong digital presence. Our team ensures your brand reaches
-                                    its target audience effectively on platforms like Facebook, Instagram, and LinkedIn.
-                                </p>
-                            </div>
-                        </div>
+                    {/* Accordion */}
+                    <div className="w-full md:w-2/5">
+                        <Accordian />
                     </div>
-
                     {/* Services Description */}
                     <div className="flex flex-col lg:w-1/2 items-center justify-end gap-3">
                         <h1 className="text-[50px] font-semibold text-left">Our Expertise</h1>
@@ -67,6 +26,8 @@ const Services = () => {
                 </div>
             </div>
         </div>
+
+
     );
 };
 
