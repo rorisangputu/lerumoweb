29cd3d5a2d12a699201054054a4dd52d979991cc
Rorisang Putu <rorisangputu@gmail.com>
2024-10-06 01:23:35 +0200

    importing framer util and addding motion

--- src/Components/About/Hero.jsx
+++ src/Components/About/Hero.jsx
@@ -1,15 +1,21 @@
-
+import { motion } from "framer-motion"
+import { FadeDown } from "../../Utils/framer"
 
 const Hero = () => {
     return (
         <div className="hero-bg py-20">
             <div className="w-[90%] mx-auto py-24">
-                <div className="flex flex-col justify-center items-center gap-7">
+                <motion.div
+                    variants={FadeDown(0.2)}
+                    initial="initial"
+                    whileInView={"animate"}
+                    viewport={{ once: true }}
+                    className="flex flex-col justify-center items-center gap-7">
                     <h1 className="text-[65px] font-semibold">About Us</h1>
                     <p className="font-light text-lg text-gray-500">
                         <span className="text-black font-semibold">Home / </span>About Us
                     </p>
-                </div>
+                </motion.div>
             </div>
         </div>
     )
