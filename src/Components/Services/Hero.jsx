c3937b12a1e87edf31513e63f2c8caf43ca75107
Rorisang Putu <rorisangputu@gmail.com>
2024-10-06 01:28:52 +0200

    adding moton to hero

--- src/Components/Services/Hero.jsx
+++ src/Components/Services/Hero.jsx
@@ -1,15 +1,22 @@
+import { motion } from "framer-motion"
+import { FadeDown } from "../../Utils/framer"
 
 
 const Hero = () => {
     return (
         <div className="hero-bg py-20">
             <div className="w-[90%] mx-auto py-24">
-                <div className="flex flex-col justify-center items-center gap-7">
+                <motion.div
+                    variants={FadeDown(0.2)}
+                    initial="initial"
+                    whileInView={"animate"}
+                    viewport={{ once: true }}
+                    className="flex flex-col justify-center items-center gap-7">
                     <h1 className="text-5xl lg:text-7xl font-semibold">Our Services</h1>
                     <p className="font-light text-lg text-gray-500">
                         <span className="text-black font-semibold">Home / </span>Services
                     </p>
-                </div>
+                </motion.div>
             </div>
         </div>
     )
