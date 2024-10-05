3c2c02707139cfca6adf6b37f736266f65a79008
Rorisang Putu <rorisangputu@gmail.com>
2024-10-06 01:29:18 +0200

    adding  motion

--- src/Components/Services/Service.jsx
+++ src/Components/Services/Service.jsx
@@ -1,6 +1,8 @@
+import { motion } from 'framer-motion';
 import code from '../../assets/code.png';
 import SocialMedia from '../../assets/SocialMedia.png';
 import WebAppImage from '../../assets/WebAppImage.png';
+import { FadeDown, SlideRight, SlideLeft } from '../../Utils/framer';
 
 const Service = () => {
     return (
@@ -9,7 +11,12 @@ const Service = () => {
                 <div className='flex flex-col-reverse items-center lg:flex-row gap-6'>
                     {/* Services */}
                     <div className="lg:w-1/2 flex flex-col gap-5">
-                        <div className='bg-white flex items-center p-6 gap-5 rounded-xl drop-shadow-md
+                        <motion.div
+                            variants={SlideLeft(0.2)}
+                            initial="initial"
+                            whileInView={"animate"}
+                            viewport={{ once: true }}
+                            className='bg-white flex items-center p-6 gap-5 rounded-xl drop-shadow-md
                             hover:drop-shadow-xl hover:scale-105 duration-200'>
                             <div className='w-1/3 h-10 lg:h-20 lg:w-20'>
                                 <img src={code} alt="" className='object-cover w-full h-full' />
@@ -22,9 +29,14 @@ const Service = () => {
                                     needs of your business while ensuring high performance and scalability.
                                 </p>
                             </div>
-                        </div>
+                        </motion.div>
 
-                        <div className='bg-blue text-white flex items-center p-6 gap-5 
+                        <motion.div
+                            variants={SlideRight(0.2)}
+                            initial="initial"
+                            whileInView={"animate"}
+                            viewport={{ once: true }}
+                            className='bg-blue text-white flex items-center p-6 gap-5 
                                 rounded-xl drop-shadow-md hover:drop-shadow-xl hover:scale-105 duration-200'>
                             <div className='w-1/3 h-10 lg:h-20 lg:w-20'>
                                 <img src={WebAppImage} alt="" className='object-cover w-full h-full' />
@@ -37,9 +49,14 @@ const Service = () => {
                                     backend solutions, we tailor every aspect to your business needs.
                                 </p>
                             </div>
-                        </div>
+                        </motion.div>
 
-                        <div className='bg-white flex items-center p-6 gap-5 rounded-xl drop-shadow-md
+                        <motion.div
+                            variants={SlideLeft(0.2)}
+                            initial="initial"
+                            whileInView={"animate"}
+                            viewport={{ once: true }}
+                            className='bg-white flex items-center p-6 gap-5 rounded-xl drop-shadow-md
                                 hover:drop-shadow-xl hover:scale-105 duration-200'>
                             <div className='w-1/3 h-10 lg:h-20 lg:w-20'>
                                 <img src={SocialMedia} alt="" className='object-cover w-full h-[80%]' />
@@ -52,18 +69,23 @@ const Service = () => {
                                     its target audience effectively on platforms like Facebook, Instagram, and LinkedIn.
                                 </p>
                             </div>
-                        </div>
+                        </motion.div>
                     </div>
 
                     {/* Services Description */}
-                    <div className="flex flex-col lg:w-1/2 items-center justify-end gap-3">
+                    <motion.div
+                        variants={FadeDown(0.2)}
+                        initial="initial"
+                        whileInView={"animate"}
+                        viewport={{ once: true }}
+                        className="flex flex-col lg:w-1/2 items-center justify-end gap-3">
                         <h1 className="text-[50px] font-semibold text-left">Our Expertise</h1>
                         <p className="text-xl max-w-xl text-center text-gray-700">
                             We specialize in building cutting-edge digital solutions tailored to help businesses thrive
                             in today’s competitive landscape. From custom websites to fully interactive web applications
-                            and comprehensive social media strategies, we've got you covered.
+                            and comprehensive social media strategies, we&apos;ve got you covered.
                         </p>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </div>
