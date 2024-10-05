773e00b6afce5f6037d6fbbe4413e2b33a33b988
Rorisang Putu <rorisangputu@gmail.com>
2024-10-06 01:24:53 +0200

    removing in component function, fetching from utility file

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -1,26 +1,8 @@
 
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
+import { FadeUp } from '../../Utils/framer';
 
-export const FadeUp = (delay) => {
-    return {
-        initial: {
-            opacity: 0,
-            y: 50,
-        },
-        animate: {
-            opacity: 1,
-            y: 0,
-            transition: {
-                type: "spring",
-                stiffness: 100,
-                duration: 0.5,
-                delay: delay,
-                ease: "easeInOut"
-            }
-        },
-    }
-}
 
 const Hero = () => {
     return (
